5ef242e412f08b07d8a4763846fd1c162f964143
Kyle Williams <48353410+Havoooo@users.noreply.github.com>
2023-07-01 20:22:07 +0100

    Add files via upload

---
+++ BankingSystem-Backend-master/TransactionScheduling1/src/main/java/com/batch/TransactionScheduling/config/UserItemProcessor.java
@@ -0,0 +1,16 @@
+package com.batch.TransactionScheduling.config;
+
+
+
+import com.common.BankData.entity.Schedule;
+import org.springframework.batch.item.ItemProcessor;
+
+
+public class UserItemProcessor implements ItemProcessor<Schedule, Schedule> {
+
+    @Override
+    public Schedule process(Schedule user) throws Exception {
+        return user;
+    }
+
+}
